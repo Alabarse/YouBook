39a63a8a6e7231c7f4eafaa41437944c4cf008b2
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-02-07 10:32:40 -0300

    first commit

---
+++ src/main/java/br/com/youbook/youbook/YoubookApplication.java
@@ -0,0 +1,13 @@
+package br.com.youbook.youbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoubookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoubookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/youbook/youbook/YoubookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.youbook.youbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoubookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
