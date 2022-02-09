e7655921110435bc8e728a3a2323b83df8a147fa
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-02-09 14:34:07 -0300

    Spring security added and changes in view templates

---
+++ src/main/java/br/com/youbook/youbook/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.youbook.youbook.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/")
+                .permitAll().anyRequest().authenticated().and().formLogin().permitAll()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.inMemoryAuthentication().withUser("Mateus").password("123").roles("ADMIN");
+    }
+    
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/imagens/**", "/css/**");
+    }
+
+}
