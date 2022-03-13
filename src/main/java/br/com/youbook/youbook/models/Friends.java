7a3ee5326c359f46562c7de331e67ab874d81f96
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-03-13 19:27:04 -0300

    Started the development of friends feature

---
+++ src/main/java/br/com/youbook/youbook/models/Friends.java
@@ -0,0 +1,18 @@
+package br.com.youbook.youbook.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Friends {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String messagesSent;
+    
+}
